b75af3564caad29db2441e6736de5b6e5392acee
plat <nonnonnonno@naver.com>
2022-04-20 11:02:09 +0900

    Add unit

--- script.js
+++ script.js
@@ -44,6 +44,21 @@ var player = {
         "concentration": { "L": -3, "T": 0, "M": 0, "I": 0, "TH": 0, "N": 1, "J": 0 },
         "mass concentration": { "L": -3, "T": 0, "M": 1, "I": 0, "TH": 0, "N": 0, "J": 0 },
         "luminance": { "L": -2, "T": 0, "M": 0, "I": 0, "TH": 0, "N": 1, "J": 0 },
+        "dynamic viscosity": { "L": -1, "T": -1, "M": 1, "I": 0, "TH": 0, "N": 0, "J": 0 },
+        "moment of force": { "L": 2, "T": -2, "M": 1, "I": 0, "TH": 0, "N": 0, "J": 0 },
+        "surface tension": { "L": 0, "T": -2, "M": 1, "I": 0, "TH": 0, "N": 0, "J": 0 },
+        "angular velocity": { "L": 0, "T": -1, "M": 0, "I": 0, "TH": 0, "N": 0, "J": 0 },
+        "angular acceleration": { "L": 0, "T": -2, "M": 0, "I": 0, "TH": 0, "N": 0, "J": 0 },
+        "heat flux density": { "L": 0, "T": -3, "M": 1, "I": 0, "TH": 0, "N": 0, "J": 0 },
+        "entropy": { "L": 2, "T": -2, "M": 1, "I": 0, "TH": -1, "N": 0, "J": 0 },
+        "specific entropy": { "L": 2, "T": -2, "M": 0, "I": 0, "TH": -1, "N": 0, "J": 0 },
+        "thermal conductivity": { "L": 1, "T": -3, "M": 1, "I": 0, "TH": -1, "N": 0, "J": 0 },
+        "energy density": { "L": -1, "T": -2, "M": 1, "I": 0, "TH": 0, "N": 0, "J": 0 },
+        "electric field strength": { "L": 1, "T": -3, "M": 1, "I": -1, "TH": 0, "N": 0, "J": 0 },
+        "electric charge density": { "L": -3, "T": 1, "M": 0, "I": 1, "TH": 0, "N": 0, "J": 0 },
+        "electric flux density": { "L": -2, "T": 1, "M": 0, "I": 1, "TH": 0, "N": 0, "J": 0 },
+        "permittivity": { "L": -3, "T": 4, "M": -1, "I": 2, "TH": 0, "N": 0, "J": 0 },
+        "permeability": { "L": 1, "T": -2, "M": 1, "I": -2, "TH": 0, "N": 0, "J": 0 },
     }
 }
 
@@ -92,6 +107,10 @@ document.getElementById("calc").addEventListener("click", function() {
         imsi = "lumen";
     } else if (player.fir == "lumen" && player.sec == "area" && player.duhap == "÷") {
         imsi = "lux";
+    } else if (player.fir == "radian" && player.sec == "second" && player.duhap == "÷") {
+        imsi = "angular velocity";
+    } else if (player.fir == "angular velocity" && player.sec == "second" && player.duhap == "÷") {
+        imsi = "angular acceleration";
     } else if (player.duhap == "×") {
         imsi = getKeyByValue(player.allElement, {
             "L": player.allElement[player.fir].L + player.allElement[player.sec].L,
@@ -114,7 +133,7 @@ document.getElementById("calc").addEventListener("click", function() {
         });
     }
     if (imsi !== undefined) {
-        document.getElementById(imsi).innerHTML = imsi;
+        if (imsi == "one") { document.getElementById(imsi).innerHTML = "1"; } else { document.getElementById(imsi).innerHTML = imsi; }
         if (!player.haveElement.includes(imsi)) { player.haveElement.push(imsi); }
         console.log("you made " + imsi);
     }
@@ -122,7 +141,7 @@ document.getElementById("calc").addEventListener("click", function() {
 
 setInterval(function() {
     if (player.fir == "one") { document.getElementById("fir").innerHTML = "1"; } else { document.getElementById("fir").innerHTML = player.fir; }
-    document.getElementById("sec").innerHTML = player.sec;
+    if (player.sec == "one") { document.getElementById("sec").innerHTML = "1"; } else { document.getElementById("sec").innerHTML = player.sec; }
     document.getElementById("duhapSelect").innerHTML = player.duhap;
     document.getElementById("haveNumber").innerHTML = "you have " + player.haveElement.length + "/" + Object.keys(player.allElement).length + "(" + (player.haveElement.length / Object.keys(player.allElement).length * 100).toFixed(2) + "%) units.";
 }, 30);
